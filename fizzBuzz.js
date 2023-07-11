f366b6de37c7336014044f39a8f21c01bbb8ae0b
Chanciier <135649126+Chanciier@users.noreply.github.com>
2023-07-10 22:43:54 -0300

    Fizz-Buzz (1 a 30)
    primeiro projeto feito sozinho

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,19 @@
-const num = 3
+for (let num = 1; num <= 30; num++) {
 
-if(num % 3 === 0){
+if(num % 3 === 0 && num % 5 === 0){
+    console.log('FizzBuzz')
+}
+
+else if(num % 3 === 0){
     console.log('Fizz')
+}
+
+else if(num % 5 === 0){
+    console.log('Buzz')
+}
+
+else{
+    console.log(num)
+}
+
 }
